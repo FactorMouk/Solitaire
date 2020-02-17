8004b2d167cbd8ddc8f4380cb69d77fc48a5c02a
myllenaalves <myllenaalves971@gmail.com>
2020-02-17 19:25:05 -0300

    Creating game selection screen base

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./home/Home";
+import SelectionScreen from "../pages/SelectionScreen/SelectionScreen";
 
 export default props => (
     <Router>
         <Switch>
             <Route path="/home" component={Home} />
-            <Route path="*" component={Home} />
+            <Route path="/selection" component={SelectionScreen} />
         </Switch>
     </Router>
 )
